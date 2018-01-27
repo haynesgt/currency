533b33c6c92257949114b99157ec06ba0482282c
Gavin <gavinh@sfu.ca>
2018-01-27 01:11:35 +0000

    Start server

---
+++ server/server.ts
@@ -0,0 +1,12 @@
+class TransactionService {
+    constructor() { }
+    public createNullTransaction() { }
+    public createTransaction(transaction, signature) { }
+}
+
+class AccountService {
+    constructor() { }
+    public createAccount(key) { }
+    public updateAccount(account) { }
+}
+
